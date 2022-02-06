5021acaf0880c19c9b2d235771a81b26ec9ca035
Otavio Ramos <otavio.omak@gmail.com>
2022-02-06 19:24:08 -0300

    Handling Errors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-const express = require('express');
+const express = require("express");
 
-const placesRoutes = require('./routes/places-routes');
+const placesRoutes = require("./routes/places-routes");
 
 const app = express();
 app.use(express.json());
 
-app.use('/api/places',placesRoutes);
+app.use("/api/places", placesRoutes);
 
+// If any route raise an error, this middleware will catch the error
+app.use((error, req, res, next) => {
+  // Verify if response was sent already
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occured!' });
+});
 
 app.listen(5000);
--- src/routes/places-routes.js
+++ src/routes/places-routes.js
@@ -1,31 +1,42 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
 const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9878531 
-        },
-        addres: '20 W 34th St, New York, NY 10001',
-        creator: 'u1'
-    }
-]
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531,
+    },
+    addres: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
+];
 
-router.get('/:placeId', (req, res, next) => {
-    const placeId = req.params.placeId;
-    const place = DUMMY_PLACES.find(place => place.id === placeId);
-    res.json({place});
+router.get("/:placeId", (req, res, next) => {
+  const placeId = req.params.placeId;
+  const place = DUMMY_PLACES.find((place) => place.id === placeId);
+  if (!place) {
+    const error = new Error("Could not find a place for the provided id.");
+    error.code = 404;
+    throw error;
+  }
+  res.json({ place });
 });
 
-router.get('/user/:userId', (req, res, next) => {
-    const userId = req.params.userId;
-    const places = DUMMY_PLACES.filter(place => place.creator === userId);
-    res.json({places});
+router.get("/user/:userId", (req, res, next) => {
+  const userId = req.params.userId;
+  const places = DUMMY_PLACES.filter((place) => place.creator === userId);
+  if (places.length === 0) {
+    const error = new Error("Could not find a place for the provided user id.");
+    error.code = 404;
+    return next(error);
+  }
+
+  res.json({ places });
 });
 
 module.exports = router;
