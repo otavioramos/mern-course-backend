3ed265151b6a911a29d7b483465ee81b2a15bba3
Otavio Santana Ramos <otavio.santana@portaldedocumentos.com.br>
2022-02-09 12:11:30 -0300

    Adding a middleware to handle 404 not found error

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 
+const HttpError = require("./models/http-error")
 const placesRoutes = require("./routes/place-routes");
 
 const app = express();
 app.use(express.json());
 
 app.use("/api/place", placesRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route', 404);
+  throw error;
+});
 
 // If any route raise an error, this middleware will catch the error
 app.use((error, req, res, next) => {
