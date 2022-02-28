75a93a3dd80c80072e920347d4abf95546c7eef1
Otavio Ramos <otavio.omak@gmail.com>
2022-02-28 19:12:08 -0300

    Implementing getAllUsers with mongodb/mongoose

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -4,32 +4,18 @@ const { validationResult } = require('express-validator');
 
 const User = require('../models/user');
 
-const DUMMY_USERS = [
-    {
-        id: 'u1',
-        name: 'Otavio',
-        image:
-            'https://upload.wikimedia.org/wikipedia/commons/e/e8/Lc3_2018_%28263682303%29_%28cropped%29.jpeg',
-        email: 'teste@teste.com',
-        password: '123456789',
-        places: [
-            {
-                id: 'p1',
-                title: 'Empire State Building',
-                description: 'One of the most famous sky scrapers in the world',
-                location: {
-                    lat: 40.7484405,
-                    lng: -73.9878531,
-                },
-                address: '20 W 34th St, New York, NY 10001',
-                creator: 'u1',
-            }
-        ],
+async function getAllUsers(req, res, next) {
+    let users;
+    try {
+        // -password exclude field 'password' from the returning object
+        users = await User.find({}, '-password');
+    } catch(err) {
+        const error = new HttpError('Fetching users failed, please try again later.', 500);
+        return next(error)
     }
-];
 
-function getAllUsers(req, res, next) {
-    res.status(200).json({users: DUMMY_USERS});
+    res.status(200);
+    res.json({users: users.map(user => user.toObject({getters: true}))});
 }
 
 async function signupUser(req, res, next) {
