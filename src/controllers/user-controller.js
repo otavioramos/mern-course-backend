061829fe37129c9ba07b8a919ce18aee40e7b882
Otavio Ramos <otavio.omak@gmail.com>
2022-04-02 00:48:10 -0300

    Hashing the password for store in the database

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,5 +1,6 @@
 const HttpError = require('../models/http-error')
 const { validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
 
 const User = require('../models/user')
 
@@ -38,11 +39,19 @@ async function signupUser(req, res, next) {
 		return next(error)
 	}
 
+	let hashedPassword 
+	try { 
+		hashedPassword = await bcrypt.hash(password, 12)
+	} catch(err) {
+		const error = new HttpError('Could not create user, please try again.', 500)
+		return next(error)
+	}
+
 	const createdUser = new User({
 		name,
 		email,
 		image: req.file.path,
-		password,
+		password: hashedPassword,
 		places: []
 	})
 
