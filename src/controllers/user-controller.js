365fe1946807fd1f9bff7d0828dc9e1405de21b6
Otavio Ramos <otavio.omak@gmail.com>
2022-04-03 19:07:20 -0300

    Changing the status code and fix typos

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -90,7 +90,7 @@ async function logInUser(req, res, next) {
 	}
 
 	if (!existingUser) {
-		const error = new HttpError('Invalid  credentials, could not log you in.',401)
+		const error = new HttpError('Invalid  credentials, could not log you in.', 403)
 		return next(error)
 	}
 
@@ -103,7 +103,7 @@ async function logInUser(req, res, next) {
 	}
 
 	if (!isValidPassword) {
-		const error = new HttpError('Invalid  credentials, could not log you in.',401)
+		const error = new HttpError('Invalid  credentials, could not log you in.', 403)
 		return next(error)
 	}
 
--- src/middlewares/check-auth.js
+++ src/middlewares/check-auth.js
@@ -10,14 +10,14 @@ module.exports = (req, res, next) => {
 	try {
 		const token = req.headers.authorization.split(' ')[1] // Authorization: 'Bearer TOKEN'
 		
-		if (!token) throw new Error('Authontication failed')
+		if (!token) throw new Error('Authentication failed')
 
 		const decodedToken = jwt.verify(token, 'supersecret_dont_share')
 		req.userData =  {userId: decodedToken.userId }
 		next()
 
 	} catch(err) {
-		const error = new HttpError('Authontication failed!', 401)
+		const error = new HttpError('Authentication failed!', 403)
 		return next(error)
 	}
 	
